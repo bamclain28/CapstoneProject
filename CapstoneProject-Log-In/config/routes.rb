b4b75847d5de6ab30c4dbfe897608e61d2626799
bamclain28 <bryanamclain@gmail.com>
2018-11-03 23:30:22 -0400

    Add files via upload

---
+++ CapstoneProject-Log-In/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+get 'log_in/index'
+get 'new_user/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
