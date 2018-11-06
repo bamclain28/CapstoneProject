24518a7685d161d52fe7ab62022a6ac3ceff0298
Bryan <bmclain5@uncc.edu>
2018-11-06 20:57:41 +0000

    Changes made to log-in and new user

--- app/controllers/new_user_controller.rb
+++ app/controllers/new_user_controller.rb
@@ -1,2 +1,4 @@
 class NewUserController < ApplicationController
+    def index
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-get 'log_in/index'
-get 'new_user/index'
+get 'log_in/log_in_index'
+get 'new_user/new_user_index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
